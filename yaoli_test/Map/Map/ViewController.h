333539923c50e2c929635f9b71793758aa59cd19
Yaoli Zheng <zhengyaoli1988@gmail.com>
2012-10-05 00:37:23 -0700

    Api request for map

--- yaoli_test/Map/Map/ViewController.h
+++ yaoli_test/Map/Map/ViewController.h
@@ -9,6 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
 #import <MapKit/MapKit.h>
+#import "CJSONDeserializer.h"
 
 @interface ViewController : UIViewController
 
--- yaoli_test/Map/Map/ViewController.m
+++ yaoli_test/Map/Map/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "CJSONDeserializer.h"
 
 @interface ViewController ()
 
@@ -27,8 +28,30 @@
     locManager.desiredAccuracy = kCLLocationAccuracyBest;
     locManager.distanceFilter = 100;
     [locManager startUpdatingLocation];
+    NSString *urlAddress=@"http://api.flickr.com/services/rest/?method=flickr.photos.getInfo&format=json&api_key=f3d3c93cac8adde7a46fc984158cd0e6&photo_id=3350763400&secret=2287e11458829fa5";
     
+    NSURL *url = [NSURL URLWithString:urlAddress];
+    //NSURLRequest *requestObj=[NSURLRequest requestWithURL:url];
+    NSError *error;
+    NSString *page = [NSString stringWithContentsOfURL:url
+                                                    encoding:NSASCIIStringEncoding error:&error];
     
+    page = [page stringByReplacingOccurrencesOfString:@"jsonFlickrApi("
+                                         withString:@""];
+    page = [page stringByReplacingOccurrencesOfString:@")"
+                                           withString:@""];
+    NSLog(@"%@", page);
+    NSData *jsonData = [page dataUsingEncoding:NSUTF32BigEndianStringEncoding];
+    error = nil;
+    NSDictionary *items = [[CJSONDeserializer deserializer] deserializeAsDictionary:jsonData error:&error];
+    NSLog(@"total items: %d", [items count]);
+    NSLog(@"error: %@", [error localizedDescription]);
+    
+    NSDictionary *photo = [items objectForKey:@"photo"];
+    NSString *id = [photo objectForKey:@"id"];
+    self.lonLabel.text = [NSString stringWithFormat: @"ID: %@", id];
+    NSString *date = [photo objectForKey:@"dateuploaded"];
+    self.latLabel.text = [NSString stringWithFormat: @"Update Date: %@", date];
 }
 
 - (void)viewDidUnload
@@ -54,15 +77,22 @@
     CLLocationCoordinate2D loc = [newLocation coordinate];
     float longitude = loc.longitude;
     float latitude = loc.latitude;
-    self.lonLabel.text = [NSString stringWithFormat:@"longtitude:  %f",longitude];
-    self.latLabel.text = [NSString stringWithFormat:@"latitude:  %f",latitude];
+    //self.lonLabel.text = [NSString stringWithFormat:@"longtitude:  %f",longitude];
+    //self.latLabel.text = [NSString stringWithFormat:@"latitude:  %f",latitude];
     self.mapView.mapType = MKMapTypeStandard;
     MKCoordinateRegion newRegion;
     newRegion.center.latitude = latitude;
     newRegion.center.longitude = longitude;
     newRegion.span.latitudeDelta = 0.004;
     newRegion.span.longitudeDelta = 0.004;
-    
+    float lo[3];
+    float la[3];
+    lo[0] = -122.4509;
+    lo[1] = -122.4505;
+    lo[2] = -122.4513;
+    la[0] = 37.7807;
+    la[1] = 37.7801;
+    la[2] = 37.7812;
     CLLocationCoordinate2D coordinate;
     coordinate.latitude = latitude;
     coordinate.longitude = longitude;
@@ -70,7 +100,20 @@
     [annotation setCoordinate:coordinate];
     [annotation setTitle:@"I am here"];
     //[self.mapView removeAnnotation:self.mapView.annotations];
+    int i;
+    for(i = 0; i < 3; i++) {
+        CLLocationCoordinate2D coordinate2;
+        coordinate2.latitude = la[i]	;
+        coordinate2.longitude = lo[i];
+        MKPointAnnotation *annotation2 = [[MKPointAnnotation new] init];
+        [annotation2 setCoordinate:coordinate2];
+        [annotation2 setTitle: [NSString stringWithFormat:@"Bank %d", i]];
+        [self.mapView addAnnotation:annotation2];
+    }
+        //[self.mapView removeAnnotation:self.mapView.annotations];
     [self.mapView addAnnotation:annotation];
-    [self.mapView setRegion:newRegion animated:YES];}
+    
+    [self.mapView setRegion:newRegion animated:YES];
+}
 
 @end
