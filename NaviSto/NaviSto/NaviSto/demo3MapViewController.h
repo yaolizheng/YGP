1966e756a890a43b21396be17e8f56aaad1e12d2
Yaoli Zheng <zhengyaoli1988@gmail.com>
2012-11-16 00:56:34 -0800

    update

---
+++ NaviSto/NaviSto/NaviSto/demo3MapViewController.h
@@ -0,0 +1,19 @@
+//
+//  demo3MapViewController.h
+//  demo
+//
+//  Created by Yaoli Zheng on 9/26/12.
+//  Copyright (c) 2012 Yaoli Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+//#import <CoreLocation/CoreLocation.h>
+#import "MBProgressHUD.h"
+
+@interface demo3MapViewController : UIViewController
+
+@property (retain, nonatomic) IBOutlet MKMapView *mapView;
+@property (retain, nonatomic) MBProgressHUD *hud;
+
+@end
